23c96df4406202b5f3d36eff9d5608eca46209ea
NguyenMinhHieu1410 <166898771+NguyenMinhHieu1410@users.noreply.github.com>
2025-02-11 10:19:19 +0700

    Update firebase.js

--- ecomResource/fe/src/utils/firebase.js
+++ ecomResource/fe/src/utils/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import { getAuth } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
 const firebaseConfig = {
-  apiKey: "AIzaSyDLzbq5tZ7Q5j1UNa_Cq5FEHDAs1mO1gIw",
-  authDomain: "kltn2024-4026c.firebaseapp.com",
-  projectId: "kltn2024-4026c",
-  storageBucket: "kltn2024-4026c.firebasestorage.app",
-  messagingSenderId: "106501602089",
-  appId: "1:106501602089:web:5f6860ef6109807ff31538"
+ "Edit this section yourself"
 };
 
 // Initialize Firebase
